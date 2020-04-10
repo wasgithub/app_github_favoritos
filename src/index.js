98c6d2d4a241ba8367064cb549acf8bb4c722819
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-04-10 20:50:50 -0300

    NAVEGACAO V4: adicionado dependencia

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  StatusBar,
-} from "react-native";
+import Routes from './routes'
 
 import "./config/ReactoTronConfig";
 
 const App = () => {
   return (
-    <>
-      <StatusBar barStyle="light-content" />
-      <Text>teste</Text>
-    </>
+    <Routes />
   );
 };
-
-const styles = StyleSheet.create({
-});
 
 export default App;
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Main from './pages/Main';
+import User from './pages/User'
+
+const Routes = createStackNavigator({
+    Main,
+    User
+});
+
+export default Routes;
