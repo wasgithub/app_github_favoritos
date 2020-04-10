af6d8cb07057683bac8d36595fa456f46b580f9a
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-04-10 19:52:54 -0300

    ESLINT: adicionado dependencia

--- App.js
+++ App.js
@@ -8,21 +8,16 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
-  View,
   Text,
   StatusBar,
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
   Colors,
-  DebugInstructions,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
+
 
 const App = () => {
   return (
