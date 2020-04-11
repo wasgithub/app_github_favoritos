fe2e9f71223d376c7cbf0ca9c1217d45218303dc
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-04-11 15:32:46 -0300

    STATUSBAR: Ajustado estilos

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Routes from './routes'
+import { StatusBar } from 'react-native'
 
 import "./config/ReactoTronConfig";
 
 const App = () => {
   return (
-    <Routes />
+    <Fragment>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
+      <Routes />
+    </Fragment>
   );
 };
 
