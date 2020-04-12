fd55771e9f1f8476c1af304e0a2d68dbf2fb0b06
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-04-12 20:15:41 -0300

    PROFILE: Ajustado layoyt de imagem e nome.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, TextInput, StyleSheet, Keyboard } from 'react-native';
+import { View, TextInput, StyleSheet, Keyboard, Image, Text, FlatList, Alert } from 'react-native';
 import { RectButton} from 'react-native-gesture-handler';
 import Icon from "react-native-vector-icons/MaterialIcons";
 
@@ -8,7 +8,7 @@ import api from '../../services/api'
 
 // import { Container } from './styles';
 
-export default function Main() {
+export default function Main(props) {
 
   const [user, setUser] = useState([])
   const [newUser, setNewUser] = useState('')
@@ -32,6 +32,10 @@ export default function Main() {
 
   }
 
+  const handleProfile = () => {
+    props.navigation.navigate("User")
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.form}>
@@ -44,11 +48,30 @@ export default function Main() {
           placeholderTextColor={"#999"}
           style={styles.input}
           returnKeyType={"send"}
+          keyboardType={"email-address"}
           onSubmitEditing={handleAddUser} />
         <RectButton style={styles.submitButton} onPress={handleAddUser}>
           <Icon name="add" size={25} color="#FFF" />
         </RectButton>
       </View>
+      <FlatList
+        style={styles.list}
+        data={user}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={user => user.login}
+        renderItem={({ item }) => {
+          return(
+            <View style={styles.user}>
+              <Image source={{uri: item.avatar_url}} style={styles.avatar_url}/>
+              <Text style={styles.name}>{item.name}</Text>
+              <Text style={styles.bio}>{item.bio}</Text>
+              <RectButton style={styles.perfilButton} onPress={handleProfile}>
+                <Text style={styles.perfilButtonText}>Ver Perfil</Text>
+              </RectButton>
+            </View>
+          )
+
+        }}/>
     </View>
   );
 }
@@ -80,5 +103,47 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginLeft: 10,
     paddingHorizontal: 12
+  },
+  list: {
+    marginTop: 20
+  },
+  user: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20
+  },
+  avatar_url: {
+    height: 84,
+    width: 84,
+    borderRadius: 41,
+    backgroundColor: "#eee"
+  },
+  name: {
+    fontSize: 14,
+    color: "#333",
+    fontWeight: "bold",
+    marginTop: 4,
+    textAlign: "center"
+  },
+  bio: {
+    textAlign: "center",
+    fontSize: 13,
+    lineHeight: 18,
+    color: "#999",
+    marginTop: 8
+  },
+  perfilButton: {
+    marginTop: 10,
+    borderRadius: 4,
+    backgroundColor: "#7159c1",
+    height: 42,
+    alignSelf: "stretch",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  perfilButtonText: {
+    color: "#fff",
+    fontSize: 14,
+    textTransform: "uppercase"
   }
 })
